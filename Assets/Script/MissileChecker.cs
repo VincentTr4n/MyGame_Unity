067af8e2db0bed12e288728fd4476092575a2861
VincentTr4n <33866720+VincentTr4n@users.noreply.github.com>
2018-03-01 20:22:39 +0700

    update enemy

---
+++ Assets/Script/MissileChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileChecker : MonoBehaviour {
+
+    public GameObject missile;
+    public Transform localStart;
+
+    private MissileControl controller;
+    private GameObject missileClone;
+
+	// Use this for initialization
+	void Start () {
+        controller = FindObjectOfType<MissileControl>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && missileClone==null)
+        {
+            missileClone = Instantiate(missile, localStart.position, localStart.rotation);
+        }
+    }
+
+}
---
+++ Assets/Script/MissileControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileControl : MonoBehaviour {
+
+    public Transform targer;
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        targer = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 direction = (Vector2)targer.position - rb.position;
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        rb.velocity = transform.up * speed;
+    }
+}
